c91a3a76f532d74cbec3ab3e92f9b80037d5e7e8
ingasun <inga.rittmann@stud.uni-due.de>
2020-11-05 00:23:12 +0100

    adjust game to definition

--- venv/game_in_simpler_gui.py
+++ venv/game_in_simpler_gui.py
@@ -56,48 +56,60 @@ exit_game_button.grid(row=9, pady=20, padx=20, sticky='NEWS')
 uc_dict = {'s1': [['t1'], ['u1', 'v1']], 'u1': [['u1']], 's2': [['t2']]}
 all_states = ['s1', 't1', 'u1', 'v1', 's2', 't2']
 bisimulation = alg.calculate_bisimulation(uc_dict, all_states)
+
+print(bisimulation)
 list_with_recently_picked_states = []
 
 
-
+# todo auch Nh aus uc zulassen
+# spiel endet, wenn duplicator nicht mit bisimularen zustand antworten kann oder Spieler 1 keinen zug machen kann
+#
 while all_states != list_with_recently_picked_states:
-
-    # hier wird glaube ich immer der gleiche Wert benutzt
-    # um an die daten zu kommen muss hier der wert vom button aufgerufen werden
-    # geht aber nicht direkt ohne button zu klicken
-    # der code wird so durchlaufen ohne auf immer neue werte zu warten
     instruction_label.config(text="Spoiler bitte Zustand wählen")
-    input_confirm_button_1.wait_variable(var_1)
-    state_1 = spoiler_input
-    if state_1 not in all_states:
-        instruction_label.config(text="Ungültiger Zustand!")
-        break
+    while True:
+        input_confirm_button_1.wait_variable(var_1)
+        state_1 = spoiler_input
+        if state_1 not in all_states:
+            print('hello')
+            instruction_label.config(text="Ungültiger Zustand, nochmal Zustand wählen Spieler 1")
+            continue
+        else:
+            break
     instruction_label.config(text="Duplicator bitte Zustand wählen")
-    input_confirm_button_2.wait_variable(var_2)
-    state_2 = duplicator_input
-    if state_2 not in all_states:
-        instruction_label.config(text="Ungültiger Zustand")
-        print('Ungültiger Zustand')
-        break
+    while True:
+        input_confirm_button_2.wait_variable(var_2)
+        state_2 = duplicator_input
+        if state_2 not in all_states:
+            instruction_label.config(text="Ungültiger Zustand, nochmal Zustand wählen Duplicator")
+            continue
+        else:
+            break
     list_with_recently_picked_states.append(state_1)
     # erstmal checken, ob die Zustände beide NH haben
     with_nh, without_nh = alg.get_list_with_without_neighbourhoods(uc_dict, all_states)
-    if (state_1 in with_nh and state_2 in without_nh) or (state_1 in without_nh and state_2 in with_nh):
-        instruction_label.config(text="Nicht bisimular, weitermachen")
-        # print('Nicht bisimular, weitermachen')
+    if state_1 in with_nh and state_2 in without_nh:
+        instruction_label.config(text="Nicht bisimular, Spoiler hat gewonnen")
+        break
+    if state_1 in without_nh and state_2 in with_nh:
+        instruction_label.config(text="Ja, die sind bisimular, Duplicator hat gewonnen")
+        break
     if state_1 in without_nh and state_2 in without_nh:
-        instruction_label.config(text="Der Zustand is da nicht drin, nochmal ")
-        # print('Ja, die sind bisimular, weitermachen')
+        instruction_label.config(text="Ja, die sind bisimular, Duplicator hat gewonnen")
+        break
+    # todo bis hier das könnte besser in extra funktion
     if state_1 in with_nh and state_2 in with_nh:
         instruction_label.config(text="jetzt NH wählen Spieler 1")
-        input_confirm_button_1.wait_variable(var_1)
-        nh_1__ = spoiler_input
-        nh_1_ = nh_1__.lstrip().split(', ')
-        nh_1 = nh_1_[0]
-        print(nh_1_)
-        if nh_1_ not in uc_dict.get(state_1):
-            instruction_label.config(text="Das ist kein NH des gewählten Zustandes")
-            # print('Das ist kein NH des gewählten Zustandes')
+        while True:
+            input_confirm_button_1.wait_variable(var_1)
+            nh_1__ = spoiler_input
+            nh_1_ = nh_1__.lstrip().split(', ')
+            nh_1 = nh_1_[0]
+            print(nh_1_)
+            if nh_1_ not in uc_dict.get(state_1):
+                instruction_label.config(text="Das ist kein NH des gewählten Zustandes, nochmal")
+                continue
+            else:
+                break
 
         # hier wird direkt abgebrochen wenn die erste uc nicht dem NH entspricht
         # for item in uc_dict.get(state_1):
@@ -106,50 +118,57 @@ while all_states != list_with_recently_picked_states:
         #         print('Das ist kein NH des gewählten Zustandes')
         #         break
         instruction_label.config(text="jetzt NH wählen Duplicator")
-        input_confirm_button_2.wait_variable(var_2)
-        nh_2__ = duplicator_input
-        nh_2_ = nh_2__.lstrip().split(', ')
-        nh_2 = nh_2_[0]
-        print(nh_2_)
-        # hier muss wahrscheinlich der input noch angepasst werden, vielleicht mit list()
-
-        if nh_2_ not in uc_dict.get(state_2):
-            instruction_label.config(text="Das ist kein NH des gewählten Zustandes")
-            # print('Das ist kein NH des gewählten Zustandes')
-            break # hier wird nur die for-Schleife beendet bis jetzt
+        while True:
+            input_confirm_button_2.wait_variable(var_2)
+            nh_2__ = duplicator_input
+            nh_2_ = nh_2__.lstrip().split(', ')
+            nh_2 = nh_2_[0]
+            print(nh_2_)
+            # hier muss wahrscheinlich der input noch angepasst werden, vielleicht mit list()
+            if nh_2_ not in uc_dict.get(state_2):
+                instruction_label.config(text="Das ist kein NH des gewählten Zustandes")
+                # print('Das ist kein NH des gewählten Zustandes')
+                continue # hier wird nur die for-Schleife beendet bis jetzt
+            else:
+                break
         # jetz muss Spieler 1 in den Nh von Spieler 2 wechseln
 
         # hier mit for Schleife überprüfen, ob die Zustände bisimular sind bis Spieler 2 keinen mehr wählen kann
-        i = 0
-        for i in range(len(nh_2) - 1):
-            i = i+1
-            test = True
-            while test:
-                instruction_label.config(text="Spieler 1: wähle Zustand aus NH von Spieler 2")
-                input_confirm_button_1.wait_variable(var_1)
-                state_in_NH_1 = spoiler_input
-                if state_in_NH_1 not in nh_2_:
-                    instruction_label.config(text="Der Zustand is da nicht drin, nochmal ")
-                    continue
-                list_with_recently_picked_states.append(state_in_NH_1)
-                instruction_label.config(text="Spieler 2: wähle Zustand aus NH von Spieler 1, der dazu bisimular ist ")
-                input_confirm_button_2.wait_variable(var_2)
-                state_in_NH_2 = duplicator_input
-                if state_in_NH_2 not in nh_1_:
-                    instruction_label.config(text="Der Zustand is da nicht drin, nochmal ")
-                    continue
-                else:
-                    test = False
-            if (state_in_NH_1, state_in_NH_2) in bisimulation:
-                instruction_label.config(text="Ja, die sind bisimular")
+        # todo das muss noch geändert werden gemäß der Definition vom Spiel
+        # i = 0
+        #for i in range(len(nh_2) - 1):
+        #    i = i+1
+        instruction_label.config(text="Spieler 1: wähle Zustand aus NH von Spieler 2")
+        while True:
+            input_confirm_button_1.wait_variable(var_1)
+            state_in_NH_1 = spoiler_input
+            if state_in_NH_1 not in nh_2_:
+                instruction_label.config(text="Der Zustand is da nicht drin, nochmal ")
                 continue
             else:
-                instruction_label.config(text="Nicht bisimular, Spieler 1 hat gewonnen")
+                list_with_recently_picked_states.append(state_in_NH_1)
+                break
+        instruction_label.config(text="Spieler 2: wähle Zustand aus NH von Spieler 1, der dazu bisimular ist ")
+        while True:
+            input_confirm_button_2.wait_variable(var_2)
+            state_in_NH_2 = duplicator_input
+            if state_in_NH_2 not in nh_1_:
+                instruction_label.config(text="Der Zustand is da nicht drin, nochmal ")
                 continue
+            else:
+                break
+        # todo hier muss nur geschaut werden, ob wieder beide NH usw.
+        # todo wird nicht als tupel erkannt
 
-        # das müsste außerhalb der while Schleife stehen, wenn nach einem Zustandspaar wirklich Schluss sein soll
-        # print('Spiel ist zuende')
-        #todo der code ist momentan unreachable
-        if list_with_recently_picked_states == all_states:
-            instruction_label.config(text="Spieler 2: wähle Zustand aus NH von Spieler 1, der dazu bisimular ist ")
-            print('Spieler 2 kann alle Zustände matchen und hat gewonnen')
+        if (state_in_NH_1, state_in_NH_2) in bisimulation:
+            print('hey')
+            instruction_label.config(text="Ja, die sind bisimular, das Spiel geht weiter")
+            if list_with_recently_picked_states == all_states:
+                instruction_label.config(text="Spieler 2 kann alle Zustände matchen und hat gewonnen")
+                break
+            else:
+                continue
+        else:
+            instruction_label.config(text="Nicht bisimular, Spieler 1 hat gewonnen")
+            # hier kann dann abgebrochen werden
+            break
